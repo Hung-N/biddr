a90ce6417e6b58643678d16925360898ec4c5b92
Hung Nguyen <reachhungnguyen@gmail.com>
2017-07-04 13:43:50 -0700

    implement navigation links add auction model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,25 @@
 Rails.application.routes.draw do
 
 
+  get 'auctions/index'
+
+  get 'auctions/new'
+
+  get 'auctions/create'
+
+  get 'auctions/show'
+
+  get 'auctions/edit'
+
+  get 'auctions/update'
+
+  get 'auctions/destroy'
+
   devise_for :users
-root to: "home#index"
+
+  root to: "home#index"
+
+  resources :auctions, only: [:index, :create, :update, :edit, :destroy]
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704195609) do
+ActiveRecord::Schema.define(version: 20170704201617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "auctions", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "title"
+    t.string "description"
+    t.integer "reserve"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_auctions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +42,5 @@ ActiveRecord::Schema.define(version: 20170704195609) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "auctions", "users"
 end
