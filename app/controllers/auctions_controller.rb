a575a995e8a2544fd1d7eed1c6e1c2e5eda48280
Hung Nguyen <reachhungnguyen@gmail.com>
2017-07-04 14:21:38 -0700

    set up ability to sign in/out/register user and create new auctions

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,11 +1,24 @@
 class AuctionsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_auction, only: [:show, :edit, :update, :destroy]
+
   def index
+    @auctions = Auction.all
   end
 
   def new
+    @auction = Auction.new
   end
 
   def create
+    @auction = Auction.new auction_params
+    @auction.user = current_user
+    if @auction.save
+      redirect_to auctions_path, notice: 'Auction created!'
+    else
+      flash.now[:alert] = 'Please fix the following errors!!!'
+      render :new
+    end
   end
 
   def show
@@ -19,4 +32,15 @@ class AuctionsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def auction_params
+    auction_params = params.require(:auction).permit([:title, :body, :category_id, :image, { tag_ids: [] }])
+  end
+
+  def find_auction
+    @auction = Auction.find params[:id]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,10 @@
 Rails.application.routes.draw do
 
-
-  get 'auctions/index'
-
-  get 'auctions/new'
-
-  get 'auctions/create'
-
-  get 'auctions/show'
-
-  get 'auctions/edit'
-
-  get 'auctions/update'
-
-  get 'auctions/destroy'
-
   devise_for :users
 
   root to: "home#index"
 
-  resources :auctions, only: [:index, :create, :update, :edit, :destroy]
+  resources :auctions, only: [:index, :new, :create, :update, :edit, :destroy]
 
 
 end
